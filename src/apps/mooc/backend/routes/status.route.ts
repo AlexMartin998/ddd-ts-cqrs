d7c2f5ee0bdc0e6743a7867d6960251ed8f6d179
Alex Martin <alextravc.342@gmail.com>
2023-04-14 11:59:57 -0500

    [ADD]: dependency injection with node-dependency-injection

--- src/apps/mooc/backend/routes/status.route.ts
+++ src/apps/mooc/backend/routes/status.route.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from 'express';
 
 import { StatusGetController } from '../controllers/StatusGetController';
-// import container from '../dependency-injection';
+import container from '../dependency-injection';
 
 export const register = (router: Router): void => {
-	const controller = new StatusGetController();
+	const controller: StatusGetController = container.get(
+		'Apps.mooc.controllers.StatusGetController'
+	);
 
 	router.get('/status', (req: Request, res: Response) => controller.run(req, res));
 };
